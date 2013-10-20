760a44fd8828f5ab905516413a2fa28e44bbf535
Piotrek <pianiel@example.com>
2013-10-21 01:00:05 +0200

    sql create script + sqlalchemy select

---
+++ ed-1.sql
@@ -0,0 +1,42 @@
+DROP TABLE IF EXISTS recipes_ingredients;
+DROP TABLE IF EXISTS recipes;
+DROP TABLE IF EXISTS ingredients;
+DROP TABLE IF EXISTS quantities;
+
+CREATE TABLE recipes (
+    id bigserial primary key,
+    name text,
+    description text,
+    url text,
+    image text,
+    source text,
+    date_published DATE,
+    recipeyield int,
+    total_time int,
+    prep_time int,
+    cook_time int
+);
+
+CREATE TABLE ingredients (
+    id bigserial primary key,
+    name text
+);
+
+CREATE TABLE quantities (
+    id bigserial primary key,
+    name text
+);
+
+CREATE TABLE recipes_ingredients (
+    id bigserial primary key,
+    recipe_id bigint NOT NULL references recipes,
+    ingredient_id bigint NOT NULL references ingredients,
+    quantity_id bigint NOT NULL references quantities,
+    quantity_count float
+);
+
+insert into recipes (name, description, url, image, source, date_published, recipeyield, total_time, cook_time, prep_time) values ('glass of water', 'just glass of water', 'www.water.com','www.water.com','www.water.com','2010-10-10',1,1,0,1);
+insert into ingredients (name) values ('water');
+insert into quantities (name) values ('glass');
+insert into recipes_ingredients (recipe_id, ingredient_id, quantity_id, quantity_count) values (1, 1, 1, 1.0);
+
--- main.py
+++ main.py
@@ -1,14 +1,42 @@
 #!/usr/bin/env python
 
+from sqlalchemy import *
 import json
+
 
 datafile = "debug20.json"
 
-def main():
+user = 'postgres'
+# password = ''
+host = 'localhost'
+dbname = 'food'
+dbstring = 'postgresql://' + user + ':' + password + '@' + host + '/' + dbname
+
+
+def fetchjson():
 	with open(datafile) as f:
 		lines = [json.loads(line) for line in f]
-		print lines[0]['ingredients']
+		# print lines[0]['ingredients']
+		print len(lines), 'records read'
+		print lines[0]
+		return lines
+
+
+def populate(records):
+	print len(records), 'records to populate'
+	db = create_engine(dbstring)
+	metadata = MetaData(db)
+	recipes = Table("recipes", metadata, autoload=True)
+	run(recipes.select())
+
+
+
+def run(stmt):
+    rs = stmt.execute()
+    for row in rs:
+        print row
 
 
 if __name__ == '__main__':
-	main()
+	records = fetchjson()
+	populate(records)
